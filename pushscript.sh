83c7f70346c45ec893d65f95e24a0dc555e62ae4
Manmeet Singh <manmeets.zsh@gmail.com>
2024-11-09 00:22:19 +0530

    app shortcuts properly working on desktop :: pwa fails to install as a PWA unlike before

--- pushscript.sh
+++ pushscript.sh
@@ -1,5 +1,5 @@
 git status --short
 git add -A
-git commit -m "signup form :: ui updates"
+git commit -m "app shortcuts properly working on desktop :: pwa fails to install as a PWA unlike before "
 git push origin main
 
--- vite.config.js
+++ vite.config.js
@@ -1,74 +1,91 @@
 import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
-   
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: false,
 
-    manifest: {
-      name: 'Burning Notes',
-      short_name: 'burningnotes',
-      description: 'A minimalist digital notebook for jotting down quick thoughts and organizing your mind.',
-      theme_color: '#111827',
-      display: "standalone", // from resources
-      "icons": [
-    {
-      "src": "/icons/burningnotes-icon-144.png",
-      "sizes": "144x144",
-      "type": "image/png"
-    },
-    {
-      "src": "/icons/burningnotes-icon-192.png",
-      "sizes": "192x192",
-      "type": "image/png"
-    },
-    {
-      "src": "/icons/burningnotes-icon-512.png",
-      "sizes": "512x512",
-      "type": "image/png"
-    }
-  ],
-      shortcuts : [
-          {
-              "name": "Create Note",
-              "url": "/add-note",
-              "description": "Create new note quickly"
-          },
-          {
-              "name": "Settings",
-              "url": "/"
-          }
-        
-      ]
-    },
+            pwaAssets: {
+                disabled: false,
+                config: true,
+            },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+            manifest: {
+                name: 'Burning Notes',
+                short_name: 'burningnotes',
+                description: 'A minimalist digital notebook for jotting down quick thoughts and organizing your mind.',
+                theme_color: '#111827',
+                display: 'standalone', // from resources
+                icons: [
+                    {
+                        src: '/icons/burningnotes-icon-144.png',
+                        sizes: '144x144',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icons/burningnotes-icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icons/burningnotes-icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                      },
+                    ],
+                shortcuts: [
+                    {
+                        name: 'Create Note',
+                        url: '/add-note',
+                        description: 'Create new note quickly',
+                        icons: [
+                          {
+                            src: '/icons/burningnotes-icon-512.png',
+                                sizes: '144x144',
+                                type: 'image/png',
+                                purpose: 'any',
+                            },
+                        ],
+                      },
+                      {
+                        name: 'Settings',
+                        url: '/',
+                        description: 'Tinker with profile',
+                        icons: [
+                          {
+                            src: '/icons/burningnotes-icon-512.png',
+                            sizes: '144x144',
+                                type: 'image/png',
+                                purpose: 'any',
+                            },
+                        ],
+                    },
+                ],
+            },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+                cleanupOutdatedCaches: true,
+                clientsClaim: true,
+            },
+
+            devOptions: {
+                enabled: false,
+                navigateFallback: 'index.html',
+                suppressWarnings: true,
+                type: 'module',
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  })],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+});
